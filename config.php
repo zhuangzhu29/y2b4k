3b6a2505bed73d2e0a910dda2cfc2aab4d56dd27
zhuangzhu29 <46889073+zhuangzhu29@users.noreply.github.com>
2019-01-22 12:09:43 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('APIKEY', 'AIzaSyB5zY2fq6YRSMxF7HwGb8rEQ8VVRxiO6ps');
+define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
 define('GJ_CODE', 'HK');
-define('SITE_NAME', 'you2b1eclub');
-define('TITLENAME', 'you2beclub');
+define('SITE_NAME', '像素彼岸');
+define('TITLENAME', '像素彼岸');
 define('EN2DEKEY', '32123wwerwertgfdgds');
 define('EMAIL', '1334347212@qq.com');
 define('NAME', 'admin');
